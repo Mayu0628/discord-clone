d602a6f63fe9eb3ebe60e358dcfba748855a6fb6
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2023-09-13 01:22:49 +0900

    サイドバー完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.scss';
-import Sidebar from './components/Sidebar';
+import React from "react";
+import "./App.scss";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
--- src/components/SidebarChannel.tsx
+++ src/components/SidebarChannel.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import './SidebarChannel.scss'
+import React from "react";
+import "./SidebarChannel.scss";
 
 const SidebarChannel = () => {
   return (
-    <div className='sidebarChannel'>
+    <div className="sidebarChannel">
       <h4>
-        <span className='sidebarChannelHash'>#</span>
+        <span className="sidebarChannelHash">#</span>
         Udemy
       </h4>
     </div>
-  )
-}
+  );
+};
 
-export default SidebarChannel
+export default SidebarChannel;
